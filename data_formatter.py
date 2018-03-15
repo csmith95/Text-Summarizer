5f6a52a06716047fd064e118f01d0144447a877d
csmithy <csmith95@stanford.edu>
2018-03-15 15:57:18 -0700

    allowed glove vocab w/o overwriting other training/vocab files

--- data_formatter.py
+++ data_formatter.py
@@ -124,10 +124,10 @@ def writeGloveEmbeddings():
 
 
 def main(unused_args):
-    assert FLAGS.split
     if FLAGS.glove == True: 
         writeGloveEmbeddings()
         return # don't overwrite other files
+    assert FLAGS.split
     assert FLAGS.in_directories and FLAGS.out_files
     output_filenames = FLAGS.out_files.split(',')
     input_directories = FLAGS.in_directories.split(',')
