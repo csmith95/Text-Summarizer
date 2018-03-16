3fd14f8947ee5438a19000a0af26b995510756b4
csmithy <csmith95@stanford.edu>
2018-03-16 01:10:18 -0700

    fixed lowercase bug...

--- data.py
+++ data.py
@@ -64,6 +64,7 @@ class Vocab(object):
         w = pieces[0]
         # if w in [SENTENCE_START, SENTENCE_END, UNKNOWN_TOKEN, PAD_TOKEN, START_DECODING, STOP_DECODING]:
         #   raise Exception('<s>, </s>, [UNK], [PAD], [START] and [STOP] shouldn\'t be in the vocab file, but %s is' % w)
+        w = w.lower()
         if w in self._word_to_id:
           raise Exception('Duplicated word in vocabulary file: %s' % w)
         self._word_to_id[w] = self._count
